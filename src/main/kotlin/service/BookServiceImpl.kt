965774ab82f12835d9f8f66cbaf7a32764ed3a6d
agadrian <agadrian@iesrafaelalberti.es>
2024-05-15 18:46:12 +0200

    correccion errores

--- src/main/kotlin/service/BookServiceImpl.kt
+++ src/main/kotlin/service/BookServiceImpl.kt
@@ -9,32 +9,32 @@ import java.util.*
  * Implementación de [IBookService] que proporciona métodos para operaciones CRUD
  * en libros utilizando un objeto que implementa [IBookDAO].
  *
- * @property userDao Objeto que implementa la interfaz [IBookDAO] para acceder a los datos de los libros.
+ * @property bookDAO Objeto que implementa la interfaz [IBookDAO] para acceder a los datos de los libros.
  */
-class BookServiceImpl(private val userDao: IBookDAO) : IBookService {
+class BookServiceImpl(private val bookDAO: IBookDAO) : IBookService {
 
     override fun create(book: Book): Book? {
-        return userDao.create(book)
+        return bookDAO.create(book)
     }
 
     override fun getById(id: UUID): Book? {
-        return userDao.getById(id)
+        return bookDAO.getById(id)
     }
 
     override fun update(book: Book): Book? {
-        return userDao.update(book)
+        return bookDAO.update(book)
     }
 
     override fun delete(id: UUID): Boolean {
-        return userDao.delete(id)
+        return bookDAO.delete(id)
     }
 
     override fun getAll(): List<Book>? {
-        return userDao.getAll()
+        return bookDAO.getAll()
     }
 
     override fun deleteAll(): Boolean {
-        return userDao.deleteAll()
+        return bookDAO.deleteAll()
     }
 
 }
